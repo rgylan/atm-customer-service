ae483ccb3d639bd3c89ef5fb4b095a1fca41731a
unknown <rgylan74@gmail.com>
2019-08-25 18:43:21 +0800

    Changed to enable discovery client.

--- src/main/java/com/rgy/atm/CustomerApp.java
+++ src/main/java/com/rgy/atm/CustomerApp.java
@@ -2,10 +2,10 @@ package com.rgy.atm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CustomerApp {
 
     public static void main(String[] args) {
